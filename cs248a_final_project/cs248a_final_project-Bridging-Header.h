a73116a984c7538048079adc75ad3109d873ca35
Rachel Naidich <rachelnaidich1@gmail.com>
2023-03-16 21:06:52 -0700

    Initial Commit

---
+++ cs248a_final_project/cs248a_final_project-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  cs248a_final_project-Bridging-Header.h
+//  cs248a_final_project
+//
+//  Created by Rachel Naidich on 3/16/23.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
